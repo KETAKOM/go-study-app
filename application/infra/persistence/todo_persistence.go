dc99449b88afc6a9916356a85dd1749aa7d0dfc6
kotake_shunki <ketakom2111178@gmail.com>
2019-08-10 13:38:18 +0900

    Mod infra todo

--- application/infra/persistence/todo_persistence.go
+++ application/infra/persistence/todo_persistence.go
@@ -1,7 +1,7 @@
 package persistence
 
 import (
-	"time"
+	"database/sql"
 
 	"github.com/KETAKOM/go-study-app/application/domain/model"
 	_ "github.com/go-sql-driver/mysql"
@@ -10,12 +10,31 @@ import (
 type TodoPersistence struct{}
 
 func (todo TodoPersistence) GetList() ([]*model.Todo, error) {
-	t := model.Todo{}
-	t.Id = 1
-	t.Title = "Golangの勉強"
-	t.Detail = "gRPC使ってみる"
-	t.Auther = "山田 太郎"
-	t.CreatedAt = time.Now()
-	t.UpdatedAt = time.Now()
-	return []*model.Todo{&t}, nil
+
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:43306)/app1")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("select id, title, detail, auther from todo")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+
+	// 構造体の型が入る配列を定義
+	ts := make([]*model.Todo, 0)
+
+	// レコード一件一件を用意した構造体に当てはめていく
+	for rows.Next() {
+		t := model.Todo{}
+		err := rows.Scan(&t.Id, &t.Title, &t.Detail, &t.Auther)
+		if err != nil {
+			panic(err.Error())
+		}
+		ts = append(ts, &t)
+	}
+	return ts, nil
 }
